2f2b0da1e95ad5349bb5988979fe07ce36cbbaa6
Pedro Perez <pedro.perez@mulesoft.com>
2019-01-21 13:58:32 +0000

    SSL test

--- src/main/java/com/mulesoft/tool/network/NetworkUtils.java
+++ src/main/java/com/mulesoft/tool/network/NetworkUtils.java
@@ -9,6 +9,9 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import javax.net.ssl.*;
+import java.security.cert.*;
+
 public class NetworkUtils {
 
 	public static String resolveIPs(String host) throws UnknownHostException {
@@ -40,33 +43,91 @@ public class NetworkUtils {
 	public static String testConnect(String host, String port) {
 		long startTime = System.nanoTime();
 		long totalTime = System.nanoTime();
-		try {
-			Socket socket = new Socket();
-			startTime = System.nanoTime();
-			socket.connect(new InetSocketAddress(host, Integer.parseInt(port)), 10000);
-			socket.setSoTimeout(10000);
-			if (socket.isConnected()) {
-				totalTime = System.nanoTime() - startTime;
-				socket.getInputStream();
+		String result = "";
+		for (int x = 1; x <= 3; x++) {
+			try {
+				Socket socket = new Socket();
+				startTime = System.nanoTime();
+				socket.connect(new InetSocketAddress(host, Integer.parseInt(port)), 10000);
+				socket.setSoTimeout(10000);
+				if (socket.isConnected()) {
+					totalTime = System.nanoTime() - startTime;
+					socket.getInputStream();
+				}
+				socket.close();
+			} 
+			catch (java.net.UnknownHostException e) {
+				return "Could not resolve host " + host;
 			}
-			socket.close();
-		} 
-		catch (java.net.UnknownHostException e) {
-			return "Could not resolve host " + host;
+			catch (java.net.SocketTimeoutException e) {
+				return "Timeout while trying to connect to " + host;
+			}
+			catch (java.lang.IllegalArgumentException e) {
+				return e.getMessage();
+			}
+			catch (Exception e) {
+				ByteArrayOutputStream b = new ByteArrayOutputStream();
+				e.printStackTrace(new PrintStream(b));
+				return b.toString();
+			}
+			result = result + "Probe " + x + ": Connection successful, RTT=" + Long.toString(totalTime/1000000) + "ms\n";
 		}
-		catch (java.net.SocketTimeoutException e) {
-			return "Timeout while trying to connect to " + host;
-		}
-		catch (Exception e) {
-			ByteArrayOutputStream b = new ByteArrayOutputStream();
-			e.printStackTrace(new PrintStream(b));
-			return b.toString();
-		}
-		return "Connection successful, RTT=" + Long.toString(totalTime/1000000) + "ms";
+		return result + "socket test completed";
 	}
 
 	public static String traceRoute(String host) throws Exception {
 		return execute(new ProcessBuilder("traceroute", "-w", "3", "-q", "1", "-m", "18", "-n", host));
+	}
+
+	public static String ssltest(String host, String port) throws Exception {
+/* 		Got stuck trying to use OpenSSL to fetch the certs. The first way it works, 
+		but the connection is kept opened and except to send to data to the remote server; 
+		so I tried to add "| echo" to emulate a return keypress, but I'm not able to run openssl that way.
+		Leavig it all here in case someone can fix it.
+
+		return execute(new ProcessBuilder("openssl", "s_client", "-showcerts", "-servername", host, "-connect", host+":"+port, "| echo"));
+		return execute(new ProcessBuilder("/bin/sh", "openssl s_client -showcerts -servername "+host+" -connect "+host+":"+port+ " | echo")); */
+
+		Certificate[] certs;
+		String result;
+		SSLSession mySSLSession;
+
+		try {
+			SSLSocketFactory factory =
+			(SSLSocketFactory)SSLSocketFactory.getDefault();
+		
+			SSLSocket socket =
+				(SSLSocket)factory.createSocket(host, Integer.parseInt(port));
+			socket.startHandshake();
+			certs = socket.getSession().getPeerCertificates();
+			mySSLSession = socket.getSession();
+			socket.close();
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+
+		result = "SSL connection results\n";
+		result = result + "======================\n";
+
+		result = result + "Negotiated protocol: " + mySSLSession.getProtocol() +"\n";
+		result = result + "Negotiated cipher suite: " + mySSLSession.getCipherSuite() + "\n\n";
+		
+		result = result + "Certificates retrieved: " + certs.length + "\n";
+		for (Certificate cert : certs) {
+			result = result + "\nCertificate\n===========\n";
+			result = result + cert.toString();
+			result = result + "";
+            if(cert instanceof X509Certificate) {
+                try {
+                    ( (X509Certificate) cert).checkValidity();
+                    result = result + "\nCertificate is active for current date\n\n";
+                } catch(CertificateExpiredException cee) {
+                    result = result + "\nCertificate is expired\n\n";
+                }
+            }
+        }
+
+		return "SSL connection successful!\n\n" + result;
 	}
 
 	private static String execute(ProcessBuilder pb) throws IOException {
